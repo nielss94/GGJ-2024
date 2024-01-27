fd4626220b9dec8ad3486866bf1a95ce4aec1f37
Niels van Dam <nielsvdam@hotmail.com>
2024-01-27 04:09:07 +0100

    reset look after turn

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -9,10 +9,10 @@ public class PlayerLook : MonoBehaviour
     private float yRotation = 0f; 
     private float xRotation = 0f;
     
-    
     [SerializeField] private float rotationSpeed = 1f; 
     [SerializeField] private Transform cameraTarget;
     
+    [SerializeField] private PlayerTurn playerTurn;
     public SnackbarInput inputActionAsset;
     
 
@@ -20,6 +20,18 @@ public class PlayerLook : MonoBehaviour
     {
         inputActionAsset = new SnackbarInput();
         inputActionAsset.Ingame.Look.Enable();
+
+        playerTurn.OnDoTurn += () =>
+        {
+            if (playerTurn.Orientation == Orientation.Snackbar)
+            {
+                yRotation = 0f;
+            }
+            else
+            {
+                yRotation = 180f;
+            }
+        };
     }
 
     private void Start()
@@ -29,22 +41,34 @@ public class PlayerLook : MonoBehaviour
 
     private void Update()
     {
+        if (playerTurn.rotating) return;
+        
         Vector2 input = inputActionAsset.Ingame.Look.ReadValue<Vector2>();
         
-        Debug.Log(input);
         // Increment the yRotation with input scaled by rotation speed
         yRotation += input.x * rotationSpeed;
+        xRotation += -input.y * rotationSpeed;
 
         // Clamp the rotation
-        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+        var clampedRotations = ClampRotationsByOrientation(playerTurn.Orientation, yRotation, xRotation);
+        yRotation = clampedRotations.Item1;
+        xRotation = clampedRotations.Item2;
 
         // Apply the rotation
         transform.rotation = Quaternion.Euler(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
-        
-        xRotation += -input.y * rotationSpeed;
-        
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        
         cameraTarget.rotation = Quaternion.Euler(xRotation, cameraTarget.eulerAngles.y, cameraTarget.eulerAngles.z);
+    }
+
+    Tuple<float, float> ClampRotationsByOrientation(Orientation orientation, float yRotation, float xRotation)
+    {
+        switch (orientation)
+        {
+            case Orientation.Snackbar:
+                return new Tuple<float, float>(Mathf.Clamp(yRotation, -90f, 90f), Mathf.Clamp(xRotation, -90f, 90f));
+            case Orientation.Kitchen:
+                return new Tuple<float, float>(Mathf.Clamp(yRotation, 90f, 270f), Mathf.Clamp(xRotation, -90f, 90f));
+            default:
+                return new Tuple<float, float>(yRotation, xRotation);
+        }
     }
 }
--- Assets/Scripts/PlayerTurn.cs
+++ Assets/Scripts/PlayerTurn.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.InputSystem;
 
 public enum Orientation
 {
@@ -12,6 +13,7 @@ public enum Orientation
 
 public class PlayerTurn : MonoBehaviour
 {
+    public event Action OnDoTurn = delegate {  };
     [SerializeField] private new Rigidbody rigidbody;
     [SerializeField] private float turnSpeedInSeconds = .5f;
 
@@ -20,42 +22,52 @@ public class PlayerTurn : MonoBehaviour
 
     [SerializeField] private bool canRotateDuringTurn = false;
     
-    private bool rotating = false;
+    public bool rotating = false;
     
     public Orientation Orientation => orientation;
     
+    public SnackbarInput inputActionAsset;
+
+    private void Awake()
+    {
+        inputActionAsset = new SnackbarInput();
+        inputActionAsset.Ingame.Turn.Enable();
+        inputActionAsset.Ingame.Turn.performed += OnTurn;
+    }
+
     private void Start()
     {
         SetOrientation(orientation);
     }
 
-    private void OnTurn()
+    private void OnTurn(InputAction.CallbackContext context)
     {
         if (rotating && !canRotateDuringTurn) return;
         
         SetOrientation(orientation == Orientation.Snackbar ? Orientation.Kitchen : Orientation.Snackbar);
     }
-    
+
     private void SetOrientation(Orientation orientation)
     {
         this.orientation = orientation;
-        
+
         switch (orientation)
         {
             case Orientation.Snackbar:
-                rigidbody.DORotate(new Vector3(0, 0, 0), turnSpeedInSeconds).SetEase(Ease.InOutSine).OnComplete(() =>
+                transform.DORotate(new Vector3(0, 0, 0), turnSpeedInSeconds).SetEase(Ease.InOutSine).OnComplete(() =>
                 {
                     rotating = false;
                 });
                 break;
             case Orientation.Kitchen:
-                rigidbody.DORotate(new Vector3(0, 180, 0), turnSpeedInSeconds).SetEase(Ease.InOutSine).OnComplete(() =>
+                transform.DORotate(new Vector3(0, 180, 0), turnSpeedInSeconds).SetEase(Ease.InOutSine).OnComplete(() =>
                 {
                     rotating = false;
                 });
                 break;
         }
-        
+
         rotating = true;
+        OnDoTurn();
     }
 }
