d337331fae39c067f6f00e3d9d1d2958c252433a
Chris <christian-boer@hotmail.com>
2024-01-27 02:24:18 +0100

    Shooting added

--- Assets/Scripts/Thrower.cs
+++ Assets/Scripts/Thrower.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Thrower : MonoBehaviour
 {
@@ -9,9 +10,17 @@ public class Thrower : MonoBehaviour
     [SerializeField] private Transform projectileSpawn;
     [SerializeField] private float throwForce = 100;
     [SerializeField] private Projector projector;
+    
 
     private void Update()
     {
         projector.SimulateTrajectory(projectilePrefab, projectileSpawn.position, projectileSpawn.forward * throwForce);
+    }
+    
+    private void OnShoot()
+    {
+        Debug.Log("Shoot");
+        var projectile = Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity);
+        projectile.Init(projectileSpawn.forward * throwForce);
     }
 }
